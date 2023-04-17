5795cbedfcb7d0d092a4f8b571172d7ddf9968ab
chaehaeun <chae_3210@naver.com>
2023-04-18 02:15:00 +0900

    [add] next.js 학습

---
+++ components/NavBar.jsx
@@ -0,0 +1,52 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import Image from "next/image";
+
+const NavBar = () => {
+  const router = useRouter();
+  return (
+    <nav>
+      <Image src="/vercel.svg" width={100} height={28} alt="dd" />
+      <div>
+        <Link href="/" className={router.pathname === "/" ? "active" : ""}>
+          Home
+        </Link>
+        <Link
+          href="/about"
+          className={router.pathname === "/about" ? "active" : ""}
+        >
+          About
+        </Link>
+      </div>
+      <style jsx>{`
+        nav {
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+        }
+        img {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
+        }
+        .active {
+          color: tomato;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
+        }
+      `}</style>
+    </nav>
+  );
+};
+
+export default NavBar;
---
+++ pages/about.jsx
@@ -0,0 +1,14 @@
+import NavBar from "@/components/NavBar";
+import React from "react";
+import Seo from "@/components/Seo";
+
+const About = () => {
+  return (
+    <div>
+      <Seo title="About" />
+      <h1>About</h1>
+    </div>
+  );
+};
+
+export default About;
